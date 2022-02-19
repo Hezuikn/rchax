813e5ccbd58a9423892e24f1a272ff7608cd67ff
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-02-19 23:38:02 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use procfs::process::{MemoryMap, Process};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::{fs::OpenOptions, os::unix::prelude::FileExt};
+use std::{env, fs::OpenOptions, os::unix::prelude::FileExt};
 
 fn main() {
-	println!("----rchax by crazy good enterprises----");
 	//x86_6 is not a typo
 	let proc = procfs::process::all_processes()
 		.unwrap()
@@ -17,6 +16,34 @@ fn main() {
 		.write(true)
 		.open(format!("/proc/{pid}/mem"))
 		.unwrap();
+	let args: Vec<String> = env::args().collect();
+	if args[1] == "off" {
+		//godmode
+		let (pos, _map) = scan(
+			&proc,
+			&hex::decode("90 90 90 90 48 8B 1C 24 4C 8B 64 24 08 4C".replace(" ", "")).unwrap(),
+		)
+		.unwrap();
+		mem_writer
+			.write_all_at(&hex::decode("41 89 47 5C".replace(" ", "")).unwrap(), pos)
+			.unwrap();
+		return;
+	}
+	if args[1] == "on" {
+		//godmode
+		let (pos, _map) = scan(
+			&proc,
+			&hex::decode("41 89 47 5C 48 8B 1C 24 4C 8B 64 24 08 4C".replace(" ", "")).unwrap(),
+		)
+		.unwrap();
+		mem_writer
+			.write_all_at(&hex::decode("90 90 90 90".replace(" ", "")).unwrap(), pos)
+			.unwrap();
+		return;
+	}
+	println!("----rchax by crazy good enterprises----");
+
+	//todo fallthrough and retry
 
 	let (pos, _map) = scan(&proc, &hex::decode("F3 0F 10 00 F3 0F 5A C0 F2 0F 5A C0 EB 24 49 8B 46 18 49 63 CC 39 48 18 0F 86 85 00 00 00 48 8D 44 C8 20 4C 63 60 04 41 83 FC FF 74 1D E9 5F FF FF FF 4C 8B 24 24 4C 8B 6C 24 08 4C 8B 74 24 10 4C 8B 7C 24 18 48 83 C4 28 C3 BF CB 06 00 02 49".replace(" ", "")).unwrap()).unwrap();
 	mem_writer
@@ -31,15 +58,6 @@ fn main() {
 		)
 		.unwrap();
 
-	let (pos, _map) = scan(
-		&proc,
-		&hex::decode("41 89 47 5C 48 8B 1C 24 4C 8B 64 24 08 4C".replace(" ", "")).unwrap(),
-	)
-	.unwrap();
-	mem_writer
-		.write_all_at(&hex::decode("90 90 90 90".replace(" ", "")).unwrap(), pos)
-		.unwrap();
-
 	println!("all done!");
 }
 
@@ -49,6 +67,7 @@ fn scan(proc: &Process, pattern: &[u8]) -> Option<(u64, MemoryMap)> {
 		.read(true)
 		.open(format!("/proc/{pid}/mem"))
 		.unwrap();
+	//todo thread local buffer
 	return proc.maps().unwrap().into_par_iter().find_map_first(|map| {
 		let (start, end) = map.address;
 		let size = end - start;
