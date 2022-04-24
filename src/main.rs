7d8ee1febb02c933c6680d4e503094df54855277
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-04-24 03:18:35 +0000

    logic, use clap

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,48 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{
 	collections::HashMap,
 	default::default,
-	env,
 	fs::{File, OpenOptions},
 	os::unix::prelude::FileExt,
 };
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+	#[clap(short, long)]
+	godmode: Option<bool>,
+}
 
 fn main() {
 	println!("----rchax by crazy good enterprises----");
-	let args: Vec<String> = env::args().collect();
+	let args = Args::parse();
 	let mut state = State::new();
 	loop {
 		//godmode
-		if let Some(x) = args.get(1) {
-			match x.as_str() {
-				"on" => {
+		if let Some(x) = args.godmode {
+			match x {
+				true => {
 					state.replace(
 						&hex!("41 89 47 5C 48 8B 1C 24 4C 8B 64 24 08 4C"),
 						&hex!("90 90 90 90"),
+						true,
 					);
 				}
-				"off" => {
+				false => {
 					state.replace(
 						&hex!("90 90 90 90 48 8B 1C 24 4C 8B 64 24 08 4C"),
 						&hex!("41 89 47 5C"),
+						true,
 					);
-				}
-				_ => {
-					panic!("undefined argument")
 				}
 			}
 		}
 
 		state.replace(&hex!("F3 0F 10 00 F3 0F 5A C0 F2 0F 5A C0 EB 24 49 8B 46 18 49 63 CC 39 48 18 0F 86 85 00 00 00 48 8D 44 C8 20 4C 63 60 04 41 83 FC FF 74 1D E9 5F FF FF FF 4C 8B 24 24 4C 8B 6C 24 08 4C 8B 74 24 10 4C 8B 7C 24 18 48 83 C4 28 C3 BF CB 06 00 02 49"),
-		&hex!("90 90 90 90"));
+		&hex!("90 90 90 90"), false);
 
 		state.replace(&hex!("48 8B C7 F3 0F 10 40 48 F3 0F 5A C0 F2 0F 5A C0 48 83 C4 08 C3 00 00 00 48 83 EC 18 48 89 7C 24"),
-		&hex!("F3 0F 10 05 F1 FF FF FF"));
+		&hex!("F3 0F 10 05 F1 FF FF FF"), false);
 
 		if !state.retry {
 			break;
@@ -80,7 +86,7 @@ impl State {
 			dry: default(),
 		}
 	}
-	fn replace(&mut self, org: &'static [u8], patch: &[u8]) {
+	fn replace(&mut self, org: &'static [u8], patch: &[u8], dont_retry: bool) {
 		if self.dry.contains_key(org) {
 			return;
 		}
@@ -89,7 +95,7 @@ impl State {
 			assert!(self.dry.insert(org, ()).is_none());
 			return;
 		}
-		self.retry = true;
+		self.retry = true && !dont_retry;
 	}
 }
 
